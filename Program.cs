eda9fa9ac84fefb201f1f4d2325de72671f678bb
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-03-28 06:22:02 +0530

    added first program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            double res = program.TestMethod();
+            Console.WriteLine("double value in main method" + res);
+
+
+            Console.Read();
+        }
+        public double TestMethod()
+        {
+            double result = 2.3 + 4.5;
+            Console.WriteLine("double value in main method" + res);
+            return result;
+        }
+    }
+}
